61aaa27062a488c7561d4fa39ed65db0b1345904
Yuewen <yuewenkapa@gmail.com>
2022-06-13 12:19:34 -0400

    contact form (without data storing)

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -16,9 +16,24 @@ public class FormHandlerServlet extends HttpServlet {
         String textValue = request.getParameter("text-input");
 
         // Print the value so you can see it in the server logs.
-        System.out.println("You submitted: " + textValue);
+        // System.out.println("You submitted: " + textValue);
 
         // Write the value to the response so the user can see it.
-        response.getWriter().println("You submitted: " + textValue);
+        // response.getWriter().println("You submitted: " + textValue);
+        // show the alert to the response so the user can see it, redirect to the page
+        String name = request.getParameter("name-input");
+        String message = request.getParameter("text-input");
+        String email = request.getParameter("email-input");
+
+        // Show in server logs.
+        System.out.println("New User submit");
+        System.out.println("user submitted: " + textValue);
+        System.out.println("user name: " + name);
+        System.out.println("user email: " + email);
+        System.out.println("User Submition Ends");
+
+        // show the alert then the user knows she/he has submmited the form successfully
+        response.getWriter()
+                .println("<script> alert('Submission Success!'); window.location.href='/contact.html' </script>");
     }
 }
